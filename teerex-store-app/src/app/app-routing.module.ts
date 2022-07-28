73250bcf58e8cf1c26af3a8c9b5971ce17cca10d
nitin chandrakar <nitin.it90@gmail.com>
2022-07-28 23:59:38 +0530

    page structure setup

--- teerex-store-app/src/app/app-routing.module.ts
+++ teerex-store-app/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {
+    'path': '',
+    'loadChildren': () => import('@modules/products/products.module').then(m => m.ProductsModule)
+   },
+   {
+    'path': 'products',
+    'loadChildren': () => import('@modules/products/products.module').then(m => m.ProductsModule)
+   },
+   {
+    'path': 'cart',
+    'loadChildren': () => import('@modules/cart/cart.module').then(m => m.CartModule)
+   },
+   { path: '**', redirectTo: 'products', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
